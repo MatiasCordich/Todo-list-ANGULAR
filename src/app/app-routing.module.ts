bf167a7e02a7014b0127170bb0c19007aa231c83
MatiasCordich <m.scordich@gmail.com>
2023-01-22 20:27:08 -0300

    deploy proyect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
-const routes: Routes = [];
+// Este arreglo contiene todas lasa rutas que vamos a renderizar
+
+const routes: Routes = [
+  {
+    path:'',
+    component: TodoListComponent
+  },
+  {
+    path: 'todo-list',
+    component: TodoListComponent
+  },
+  {
+    path:'todo-list/create',
+    component: TodoFormComponent
+  },
+  {
+    path:'todo-list/edit/:id',
+    component: TodoFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TodoListComponent,
+    TodoFormComponent
   ],
   imports: [
     BrowserModule,
